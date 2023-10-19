b193cf46e5a6164819fa92e7691dd78676a56710
Nithin <nithinkatkam504106@gmail.com>
2023-10-19 18:12:18 +0530

    push initalised

--- pushfid/app/page.js
+++ pushfid/app/page.js
@@ -1,113 +1,103 @@
-import Image from 'next/image'
+'use client'
+  import { PushAPI } from '@pushprotocol/restapi';
+import { send } from '@pushprotocol/restapi/src/lib/chat';
+  import { createSocketConnection, EVENTS } from '@pushprotocol/socket';
+  import { ethers } from 'ethers';
+  import { useEffect, useState } from 'react';
+  // Creating a random signer from a wallet, ideally this is the wallet you will connect
+  // const signer = ethers.Wallet.createRandom();
 
-export default function Home() {
+
+// const signerS = ethers.Wallet.createRandom()
+// const userAlice = await PushAPI.initialize(signerS, { env: 'staging' })
+// const response = await userAlice.channel.create({
+//   name: 'DEMO Test Channel',
+//   description: 'this is a demo testchannel',
+//   icon: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAz0lEQVR4AcXBsU0EQQyG0e+saWJ7oACiKYDMEZVs6GgSpC2BIhzRwAS0sgk9HKn3gpFOAv3v3V4/3+4U4Z1q5KTy42Ql940qvFONnFSGmCFmiN2+fj7uCBlihpgh1ngwcvKfwjuVIWaIGWKNB+GdauSk8uNkJfeNKryzYogZYoZY40m5b/wlQ8wQM8TayMlKeKcaOVkJ71QjJyuGmCFmiDUe+HFy4VyEd57hx0mV+0ZliBlihlgL71w4FyMnVXhnZeSkiu93qheuDDFDzBD7BcCyMAOfy204AAAAAElFTkSuQmCC',
+//   url: 'https://push.org',
+// })
+//   // This will be the wallet address of the recipient 
+//   const bobWalletAddress = "0x99A08ac6254dcf7ccc37CeC662aeba8eFA666666";
+
+//   // Send a message to Bob
+//   const aliceMessagesBob = await userAlice.chat.send(bobWalletAddress, {
+//     content: "Gm gm! It's a me... Mario"
+//   });
+
+//   // Create Socket to Listen to incoming messages
+//   const pushSDKSocket = createSocketConnection({
+//     user: signer.wallet,
+//     socketType: 'chat',
+//     socketOptions: { autoConnect: true, reconnectionAttempts: 3 },
+//     env: 'staging',
+//   });
+
+//   // React to message payload getting received
+//   pushSDKSocket?.on(EVENTS.CHAT_RECEIVED_MESSAGE, (message) => {
+//     console.log("hit"+ message);
+//   });
+ function Home() {
+
+
+const [state, setState] = useState({
+  provider:null,
+  signer:null
+
+});
+const [account, setAccount] = useState('None');
+const connectWallet = async () => {
+  try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+          const accounts = await ethereum.request({
+              method: 'eth_requestAccounts',
+          });
+
+          window.ethereum.on('chainChanged', () => {
+              window.location.reload();
+          });
+
+          window.ethereum.on('accountsChanged', () => {
+              window.location.reload();
+          });
+          const provider = new ethers.providers.Web3Provider(ethereum);
+          const signer = provider.getSigner();
+
+          setAccount(accounts[0]);
+          setState({ provider, signer });
+          console.log("there is" + state.signer)
+      } else {
+          alert('Please install MetaMask');
+      }
+  } catch (error) {
+      console.log(error);
+  }
+};
+
+const sendMesage=async()=>{
+    const userA= await PushAPI.initialize(state.signer, { env: 'staging' });
+    const response = await userA?.channel?.create({
+      name: 'asdasdel',
+      description: 'Tasdasdasdasdaest Description',
+      icon: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAz0lEQVR4AcXBsU0EQQyG0e+saWJ7oACiKYDMEZVs6GgSpC2BIhzRwAS0sgk9HKn3gpFOAv3v3V4/3+4U4Z1q5KTy42Ql940qvFONnFSGmCFmiN2+fj7uCBlihpgh1ngwcvKfwjuVIWaIGWKNB+GdauSk8uNkJfeNKryzYogZYoZY40m5b/wlQ8wQM8TayMlKeKcaOVkJ71QjJyuGmCFmiDUe+HFy4VyEd57hx0mV+0ZliBlihlgL71w4FyMnVXhnZeSkiu93qheuDDFDzBD7BcCyMAOfy204AAAAAElFTkSuQmCC',
+      url: 'https://push.org',
+    })
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <div>
+      {account !== 'None' ? (
+                <p>Connected Account: {account}</p>
+            ) : (
+                <button onClick={()=>connectWallet()}>Metamask</button>
+            )}
+                </div>
+                Create a Channel
+                <button className='m-1 p-2 bg-slate-500' onClick={()=>sendMesage()}>Create</button>
+     </div>
     </main>
   )
 }
+export default Home
