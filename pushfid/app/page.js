aa41eb4366a6cdc31ccffefdc24b3d9080c82e90
Nithin <nithinkatkam504106@gmail.com>
2023-10-22 19:19:11 +0530

    added further

--- pushfid/app/page.js
+++ pushfid/app/page.js
@@ -4,12 +4,10 @@ import { send } from '@pushprotocol/restapi/src/lib/chat';
   import { createSocketConnection, EVENTS } from '@pushprotocol/socket';
   import { ethers } from 'ethers';
   import { useEffect, useState } from 'react';
-
+  const [aliceChats, setAliceChats] = useState([]);
   import { CovalentClient } from "@covalenthq/client-sdk";
 
  function Home() {
-
-
 const [state, setState] = useState({
   provider:null,
   signer:null
@@ -79,6 +77,12 @@ let addressExists = false;
 }
 
 
+const chatList = async () => {
+  const userA = await PushAPI.initialize(state.signer, { env: 'staging' });
+  const chats = await userA?.chat?.list("CHATS");
+  setAliceChats(chats || []);
+};
+
 const CreateGroup=async()=>{
   const userA= await PushAPI.initialize(state.signer, { env: 'staging' });
 
@@ -115,18 +119,82 @@ const createChannel=async()=>{
 }
 const sendMesage=async()=>{
     const userA= await PushAPI.initialize(state.signer, { env: 'staging' });
-    // const response = await userA?.channel?.create({
-    //   name: 'ETHDEMO',
-    //   description: 'This is demo channel which ',
-    //   icon: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAz0lEQVR4AcXBsU0EQQyG0e+saWJ7oACiKYDMEZVs6GgSpC2BIhzRwAS0sgk9HKn3gpFOAv3v3V4/3+4U4Z1q5KTy42Ql940qvFONnFSGmCFmiN2+fj7uCBlihpgh1ngwcvKfwjuVIWaIGWKNB+GdauSk8uNkJfeNKryzYogZYoZY40m5b/wlQ8wQM8TayMlKeKcaOVkJ71QjJyuGmCFmiDUe+HFy4VyEd57hx0mV+0ZliBlihlgL71w4FyMnVXhnZeSkiu93qheuDDFDzBD7BcCyMAOfy204AAAAAElFTkSuQmCC',
-    //   url: 'https://push.org',
-    // })
-  // await userAlice.chat.group.join(chatid)
 
-    const joinGroup = await userA?.chat?.group.join("a0acaa1c3bce0a0114895903bbf229c16f4d64dd0c4749b7022b5de185f936af");
+  const createTokenGatedGroup = await userA.chat.group.create('FI on PUSHFI', {
+    description: 'Token gated web3 native chat example', // provide short description of group
+    image: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAuUlEQVR4AcXBIY5CMRiF0e/ddA1YXLeAHINGNyMwDasitZNqNGa2MLKOXeDe2F+VvBByz1nOu+dK8HP/4pO+j79EwkyYCbMl/R1WJnrNvKO0wYwwE2bCLLFRaYOZXjNbCDNhJsyEmTATZsIs9ZqJSht8Uq+ZSJgJM2GWeKG0QdRrZqa0wRbCTJgJs1TaIOo1E5U2eEevmai0QSTMhJkwW86758rEaX8huj2uzJz2F6Lb48qMMBNmwuwfJS4m9/9SUGcAAAAASUVORK5CYII=', // provide base64 encoded image
+    members: [],
+    admins: [], // not needed as per problem statement, can omit
+    private: true,
+    "rules": {
+      "entry": { 
+        "conditions": { 
+          "any": [ 
+            { 
+              "any": [ 
+                { // criteria 1
+                  "type": "PUSH",
+                  "category": "INVITE",
+                  "subcategory": "DEFAULT",
+                  "data": {
+                      "inviterRoles": [
+                          "ADMIN",
+                          "OWNER"
+                      ]
+                  }
+                }
+              ]
+            },
+            {
+              "any": [ // decider 2 - If user has $PUSH on Ethereum or on Polygon
+                { // criteria object
+                  "type": "PUSH", // define type that rules engine should go for
+                  "category": "ERC20", // define it's ERC20 token that you want to check, supports ERC721 as well
+                  "subcategory": "holder", // define if you are checking 'holder' or 'owner'
+                  "data": { 
+                    "contract": "eip155:137:0x2b9bE9259a4F5Ba6344c1b1c07911539642a2D33", // $PUSH address on ETH
+                    "comparison": ">=", // what comparison needs to pass
+                    "amount": 12, // amount that needs to passed
+                    "decimals": 18, // the decimals for the token
+                  }
+                },
+              ]
+            }
+          ]
+        }
+      }
+    },
+    "chat": {
+      "conditions": { // define condition for sending message in the group
+        "all": [
+          {
+            "any": [ // set decider to 'any', if 'and' then all rules need to be fulfilled
+              { // define criteria 1
+                "type": "PUSH", // define type that rules engine should go for, currently supports PUSH or GUILD
+                "category": "ERC20", // define it's ERC20 token that you want to check, supports ERC721 as well
+                "subcategory": "holder", // define if you are checking 'holder' or 'owner'
+                "data": { // define the data check
+                  "contract": "eip155:1:0xBE18197d1c071b72fb2460B1652C96C22d40F1D9", // pass {blockchain_standard}:{chain_id}:{address} as a shorthand
+                  "comparison": "<=", // what comparison needs to pass
+                  "amount": 1000, // amount that needs to passed
+                  "decimals": 18, // the decimals for the contract
+                }
+              }
+            ]
+          }
+        ]
+      }
+    }
+  });
+  console.log("so token"+createTokenGatedGroup.chatId + " "+ createTokenGatedGroup);
+  // await userAlice.chat.group.join(chatid)
   }
-  // await userAlice.chat.group.join(chatid)
 
+const createwithchannelId=async()=>{
+  const userA= await PushAPI.initialize(state.signer, { env: 'staging' });
+  const joinGroup = await userA.chat.group.join('79d3b055d0ea15bbaba1577fe4812ba8f9e66e71b9cfeef5bab9ee9e123b74db');
+  console.log("sgroup" +joinGroup);
+}
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -139,14 +207,25 @@ const sendMesage=async()=>{
                 </div>
                 Create a Channel
               
-                <button className='m-1 p-2 bg-slate-500' onClick={()=>sendMesage()}>join</button>
+                <button className='m-1 p-2 bg-slate-500' onClick={()=>sendMesage()}>CreateDAO</button>
      </div>
+     <ul>
+        {aliceChats.map((chat, did) => (
+          <li key={did}>
+            <div>Chat ID: {chat.chatId}</div>
+            <div>Abouts: {chat.did}</div>
+            {/* Add more properties you want to display */}
+          </li>
+        ))}
+      </ul>
      <div>
      <button className='m-1 p-2 bg-slate-600' onClick={()=>CreateGroup()}>Creategroup</button>
      </div>
      <div>
      <button className='m-1 p-2 bg-slate-600' onClick={()=>createChannel()}>createChannel</button>
      <button className='m-1 p-2 bg-slate-600' onClick={()=>ApiServices()}>Fetch details</button>
+     <button className='m-1 p-2 bg-slate-600' onClick={()=>createwithchannelId()}>Fetch details</button>
+     <button className='m-1 p-2 bg-slate-600' onClick={()=>chatList()}>Fetch details</button>
      </div>
     </main>
   )
